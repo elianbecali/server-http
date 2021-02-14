882e41d41c9bd7b16f44729078abc4f2f8e2aa8b
Elian Becali <elianbecaliaguiar@gmail.com>
2021-02-14 09:11:02 -0400

    refactor: add query params

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const http = require('http');
+const url = require('url');
 
 const routes = require('./routes');
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = url.parse(request.url, true);
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === request.url && routeObj.method === request.method
+    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
   ));
 
   if (route) {
+    request.query = parsedUrl.query;
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.end(`Cannot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
   }
 });
 
